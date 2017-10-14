68a3a4397510dd7d3f38aa13c57996edd07530ee
Zhong Chu <zhongchu@usc.edu>
2017-10-13 20:01:48 -0700

    Transaction model Singleton

--- Sanity/app/src/main/java/Model/Transaction.java
+++ Sanity/app/src/main/java/Model/Transaction.java
@@ -6,9 +6,42 @@ package Model;
 
 public class Transaction {
     private double mAmount;
-    private String mCategory;
-    public Transaction(double amount, String category){
+    private long mCategoryId;
+    private long mTransactionId;
+    private String mNotes;
+
+    public Transaction(double amount, long categoryId, long transID, String notes){
         mAmount = amount;
-        mCategory = category;
+        mCategoryId = categoryId;
+        mTransactionId = transID;
+        mNotes = notes;
     }
+
+    public double GetAmount(){
+        return mAmount;
+    }
+
+    public void SetmAmount(double amount){
+        mAmount = amount;
+    }
+
+    public long GetCategoryId(){
+        return mCategoryId;
+    }
+
+    public void SetmCategoryId(long categoryId){
+        mCategoryId = categoryId;
+    }
+
+    public long GetTransactionId(){
+        return mTransactionId;
+    }
+
+    public void SetmTransactionId(long transID){
+        mTransactionId = transID;
+    }
+
+    public String GetNotes() { return mNotes; }
+
+    public void SetmNotes(String notes) { mNotes = notes; }
 }
