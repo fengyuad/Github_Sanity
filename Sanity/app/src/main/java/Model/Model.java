0c76a5d8398260b31c98d46ef589d7e7169de51a
Bingtang Wang <bingtanw@usc.edu>
2017-10-15 21:39:16 -0700

    Update Model Class

--- Sanity/app/src/main/java/Model/Model.java
+++ Sanity/app/src/main/java/Model/Model.java
@@ -2,11 +2,34 @@ package Model;
 
 import com.google.firebase.database.DatabaseReference;
 
+import java.io.Serializable;
+
 /**
  * Created by Yifan on 10/13 013.
  */
 
-public class Model {
+public class Model implements Serializable{
     protected String mUserID = "60WQP281jXVqW57q3JP6vQeMeq73";
-    protected DatabaseReference mDatabase;
+    protected transient DatabaseReference mDatabase;
+    protected double mThreshold = 0.75;
+    protected int mFrequency = 1;
+
+    /**
+     * Getter and Setter
+     */
+    public double getmThreshold() {
+        return mThreshold;
+    }
+
+    public void setmThreshold(double mThreshold) {
+        this.mThreshold = mThreshold;
+    }
+
+    public int getmFrequency() {
+        return mFrequency;
+    }
+
+    public void setmFrequency(int mFrequency) {
+        this.mFrequency = mFrequency;
+    }
 }
