656a57ab60519c9eaa7fc7ad52c7ea70ef01c60e
Fengyuan Dong <fengyuad@usc.edu>
2017-10-16 23:22:21 -0700

    fix_edit_budget

--- Sanity/app/src/main/java/com/example/tomdong/sanity/BudgetViewActivity.java
+++ Sanity/app/src/main/java/com/example/tomdong/sanity/BudgetViewActivity.java
@@ -9,15 +9,18 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.baoyz.swipemenulistview.SwipeMenu;
 import com.baoyz.swipemenulistview.SwipeMenuCreator;
@@ -28,6 +31,11 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import Model.BudgetModel;
+import Model.Category;
+
+import static android.content.ContentValues.TAG;
+
 /**
  * Created by tomdong on 10/13/17.
  */
@@ -36,9 +44,12 @@ public class BudgetViewActivity extends AppCompatActivity implements Button.OnCl
     private ProgressBar BudgetProgress;
     private ListView CateGory_ListView;
     private TextView BudgetPercent;
-
+    private EditText edit_cat;
+    private EditText edit_buddget_name;
+    private Long budget_id;
     Button editBgtDateButton;
     TextView editBgtDateText;
+    SwipeMenuListView lv;
     private int editYear, editMonth, editDay;
 
     @Override
@@ -65,7 +76,7 @@ public class BudgetViewActivity extends AppCompatActivity implements Button.OnCl
         editDay = c.get(Calendar.DAY_OF_MONTH);
         editMonth = c.get(Calendar.MONTH);
         editYear = c.get(Calendar.YEAR);
-
+        budget_id=getIntent().getLongExtra("bgt_id",0);
         FloatingActionButton bgt_fab = (FloatingActionButton) findViewById(R.id.bgt_edit_fab);
         bgt_fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,49 +97,19 @@ public class BudgetViewActivity extends AppCompatActivity implements Button.OnCl
         editBgtDateText = (TextView) promptView.findViewById(R.id.edit_bgt_date);
         editBgtDateButton = (Button) promptView.findViewById(R.id.edit_bgt_date_button);
         editBgtDateButton.setOnClickListener(this);
-        SwipeMenuListView lv=promptView.findViewById(R.id.budget_edit_catgoryList);
+        edit_cat=promptView.findViewById(R.id.edit_catgory_name);
+        edit_buddget_name=promptView.findViewById(R.id.edit_bgt_name);
+        lv=promptView.findViewById(R.id.budget_edit_catgoryList);
         // setup a dialog window
-        ArrayList<Category_card>testlist=new ArrayList<>();
-        testlist.add(new Category_card("working"));
-        testlist.add(new Category_card("playing"));
-        testlist.add(new Category_card("dining"));
-        final ArrayAdapter<Category_card> adapter=new ArrayAdapter<Category_card>(BudgetViewActivity.this,android.R.layout.simple_list_item_1,testlist);
-        lv.setAdapter(adapter);
-        SwipeMenuCreator creator = new SwipeMenuCreator() {
-
-            @Override
-            public void create(SwipeMenu menu) {
-
-                // create "delete" item
-                SwipeMenuItem deleteItem = new SwipeMenuItem(
-                        getApplicationContext());
-                // set item background
-                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
-                        0x3F, 0x25)));
-                // set item width
-                deleteItem.setWidth(170);
-                menu.addMenuItem(deleteItem);
-
-
-            }
-        };
-        lv.setMenuCreator(creator);
-        lv.setSwipeDirection(SwipeMenuListView.DIRECTION_LEFT);
-
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position,
-                                    long id) {
-
-                editBgtDateText.setText(adapter.getItem(position).GetCatoryType());
-            }
-        });
-
-
+       GetCategoriesShows();
         alertDialogBuilder.setCancelable(false)
                 .setPositiveButton("Submit", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-
+                       if(edit_cat.getText().toString().isEmpty()||edit_buddget_name.getText().toString().isEmpty()
+                               ||editBgtDateText.getText().toString().isEmpty())
+                       {
+                           Toast.makeText(BudgetViewActivity.this, "Email sent!!!", Toast.LENGTH_SHORT).show();
+                       }
                     }
                 })
                 .setNegativeButton("Cancel",
@@ -153,5 +134,60 @@ public class BudgetViewActivity extends AppCompatActivity implements Button.OnCl
             }
         }, editYear, editMonth, editDay);
         datePickerDialog.show();
+    }
+
+    public void GetCategoriesShows()
+    {
+        Long temp=1508211333309L;
+        budget_id=temp;
+        BudgetModel bmodel=BudgetModel.GetInstance();
+       ArrayList<Category>nlist= (ArrayList<Category>) bmodel.getCategoriesUnderBudget(budget_id);
+        ArrayList<String>catNames=new ArrayList<>();
+        for(int i=0;i<nlist.size();i++)
+        {
+            catNames.add(nlist.get(i).getmName());
+        }
+        final ArrayAdapter<String> adapter=new ArrayAdapter<String>(BudgetViewActivity.this,android.R.layout.simple_list_item_1,catNames);
+        lv.setAdapter(adapter);
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(170);
+                menu.addMenuItem(deleteItem);
+            }
+        };
+        lv.setMenuCreator(creator);
+        lv.setSwipeDirection(SwipeMenuListView.DIRECTION_LEFT);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+
+                edit_cat.setText(adapter.getItem(position));
+            }
+        });
+
+        lv.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index) {
+                    case 0:
+                        Log.e(TAG, "Delete Position: " + position);
+                        adapter.remove(adapter.getItem(position));
+                        break;
+                }
+                // false : close the menu; true : not close the menu
+                return false;
+            }
+        });
     }
 }
