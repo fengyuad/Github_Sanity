1faf88a7f891e2cdd38978550e9051cf79b97d4e
Bingtang Wang <bingtanw@usc.edu>
2017-11-19 22:55:07 -0800

    Add some functions in SharedBudget

--- Sanity/app/src/main/java/Model/SharedBudgetModel.java
+++ Sanity/app/src/main/java/Model/SharedBudgetModel.java
@@ -1,5 +1,7 @@
 package Model;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.*;
 
 /**
@@ -10,9 +12,11 @@ public class SharedBudgetModel extends Model {
     private static SharedBudgetModel mInstance = null;
     private Map<Long, SharedBudget> mIDTOSharedBudget;
 
+
     private SharedBudgetModel(){
         super();
         mIDTOSharedBudget = new HashMap<>();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("SharedBudget");
 
     }
 
@@ -23,5 +27,28 @@ public class SharedBudgetModel extends Model {
         return mInstance;
     }
 
-    
+    public void AddSharedUserToBudget(long userID, long budgetID){
+        mIDTOSharedBudget.get(budgetID).AddSharedUser(userID);
+    }
+
+    public SharedBudget GetSharedBudgetByID(long budgetID){
+        return mIDTOSharedBudget.get(budgetID);
+    }
+
+    public SharedBudget CreateNewSharedBudget(String name, double amountLimit, long mDueTime, int period){
+        SharedBudget sb = new SharedBudget();
+        sb.setmName(name);
+        sb.setmAmountLimit(amountLimit);
+        sb.setmDueTime(mDueTime);
+        sb.setmBudgetID(System.currentTimeMillis());
+        sb.setmPeriod(period);
+        mIDTOSharedBudget.put(sb.getmBudgetID(), sb);
+        return sb;
+    }
+
+    public void RemoveSharedUserFromBudget(long userID, long budgetID){
+        mIDTOSharedBudget.get(budgetID).RemoveSharedUser(userID);
+    }
+
 }
+
